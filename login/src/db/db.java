bf3be4f8bda4e3a7c09bc47b8d47640018f808ba
Pooja Singh Yadav <113007377+pooja831888@users.noreply.github.com>
2022-09-26 12:38:23 +0530

    Add files via upload

---
+++ login/src/db/db.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package db;
+import java.sql.*;
+
+/**
+ *
+ * @author sypoo
+ */
+public class db {
+    public Connection con;
+    public Statement st;
+    public ResultSet rst;
+    public PreparedStatement pst;
+    
+    public db()
+    {
+    
+    try
+    {
+        
+        Class.forName("com.mysql.jdbc.Driver");
+        con=DriverManager.getConnection("jdbc:mysql://localhost:3306/login","root","root");
+        
+    }
+    catch(Exception e)
+    {
+        e.printStackTrace();
+    }
+    
+    
+}
+}
